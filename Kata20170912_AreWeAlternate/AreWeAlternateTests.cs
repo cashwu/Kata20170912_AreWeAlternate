26601bb513ebd6065c83e6def1fa4db1902eeafe
cash <cash@cashwu.com>
2017-09-12 17:37:54 +0800

    input bcd test case

--- Kata20170912_AreWeAlternate/AreWeAlternateTests.cs
+++ Kata20170912_AreWeAlternate/AreWeAlternateTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170912_AreWeAlternate
     public class AreWeAlternateTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_bcd_should_return_false()
         {
+            IsAltShouldBeFalse("bcd");
+        }
+
+        private static void IsAltShouldBeFalse(string word)
+        {
+            var kata = new Kata();
+            var actual = kata.IsAlt(word);
+            Assert.IsFalse(actual);
+        }
+    }
+
+    public class Kata
+    {
+        public bool IsAlt(string word)
+        {
+            return false;
         }
     }
 }
